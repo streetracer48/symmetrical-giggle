148a5258459027ef56d9b3a3ba777f911b246af2
karim <karim@csit.fi>
2020-04-16 20:36:02 +0300

    layouts design setup

---
+++ src/layouts/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+
+    <Link className="navbar-brand" to="/">Breeds <span className="sr-only">(current)</span></Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/breeds">Breeds</Link>
+        </li>
+      </ul>
+      
+    </div>
+    </div>
+  </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/layouts/SearchArea.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+const SearchArea = () => {
+  return (
+    <section className="hero-section pt-5 pb-5">
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="search">
+                        <div className="card">
+                            <div className="card-body">
+                                <h3 className="card-title mb-3">Find Your Perfect Cat Breed</h3>
+                                <form >
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default SearchArea;
