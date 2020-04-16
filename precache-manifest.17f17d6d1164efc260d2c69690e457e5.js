398eb2ffe5dd78d95f3ad295537c58190330a993
karim <karim@csit.fi>
2020-04-17 02:59:37 +0300

    Updates

---
+++ precache-manifest.17f17d6d1164efc260d2c69690e457e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb2fe914e4dddef78cccaa9a0bd4a32",
+    "url": "/symmetrical-giggle/index.html"
+  },
+  {
+    "revision": "0ba395173a5c01e0f319",
+    "url": "/symmetrical-giggle/static/css/main.5d6d484c.chunk.css"
+  },
+  {
+    "revision": "c1b86f18e65e033a39a5",
+    "url": "/symmetrical-giggle/static/js/2.0af82f44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/symmetrical-giggle/static/js/2.0af82f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba395173a5c01e0f319",
+    "url": "/symmetrical-giggle/static/js/main.f80869bb.chunk.js"
+  },
+  {
+    "revision": "b18e8335cddc6c9ed421",
+    "url": "/symmetrical-giggle/static/js/runtime-main.4e63a8cb.js"
+  },
+  {
+    "revision": "ac4471291c620d8dd47697a1d8da4975",
+    "url": "/symmetrical-giggle/static/media/spinner.ac447129.gif"
+  }
+]);
